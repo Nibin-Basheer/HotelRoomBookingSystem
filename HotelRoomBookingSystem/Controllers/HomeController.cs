428603482a3628245fdbccbf35de3b6c26c78309
NIBIN BASHEER <nibinerikkil@gmail.com>
2023-10-10 22:39:26 +0530

    completed register,login,about us and contact us actions

--- HotelRoomBookingSystem/Controllers/HomeController.cs
+++ HotelRoomBookingSystem/Controllers/HomeController.cs
@@ -9,18 +9,34 @@ namespace HotelRoomBookingSystem.Controllers
     public class HomeController : Controller
     {
         /// <summary>
-        /// This is Main home page design
+        /// This is Main home page action
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// This is login page action
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Login()
         {
             return View();
         }
+        /// <summary>
+        /// This is register page action
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
         {
             return View();
         }
